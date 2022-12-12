9e26f1eec1f50e6727eb6e664252b46780cab6d4
jessicaluane <jessicaluane@yahoo.com.br>
2022-12-11 21:03:25 -0300

    Changing interface

--- src/components/Button/types.ts
+++ src/components/Button/types.ts
@@ -1,4 +1,7 @@
 export interface IButtonProps {
     title: string;
+    type: string;
+    disabled: boolean;
+    children?: React.ReactNode | React.ReactNode[]
     onClick?: React.MouseEventHandler<HTMLButtonElement>;
 }
